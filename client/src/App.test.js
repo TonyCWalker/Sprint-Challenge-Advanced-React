72b6db91de55739c0487ec9be59cbc4bd1359808
Tony Walker <Walkertony509@gmail.com>
2020-03-20 20:11:45 -0700

    I have added some tests.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+import NavBar from './components/Navbar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  render(<App />);
+});
+
+test('First Test', () => {
+  const test = render(<App />)
+  test.getByTestId(/MainApp/i)
+});
+
+test("Navbar", () => {
+  render(<NavBar />);
+});
+
+test('should have text world cup', ()=>{
+  const {getByText} = render(<App />);
+  getByText(/world cup/i);
 });
